29e26c8b4564437010520fcf68ddb2dc6c19c480
slimbde@gmail.com <grigoriydolgiy@mechel.ru>
2021-09-08 15:23:29 +0500

    authentication

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,9 +1,5 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Collections.Specialized;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
